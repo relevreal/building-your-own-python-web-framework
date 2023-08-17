3f3e605b6c2c7d5c7500edbdc91869877e45d319
relevreal <relevreal@protonmail.com>
2023-08-17 14:46:14 +0200

    add request handlers and simple parametrized routing

---
+++ app.py
@@ -0,0 +1,21 @@
+from api import API
+
+app = API()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello form the HOME page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello form the ABOUT page"
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
+
